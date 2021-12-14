564b8340612836d80a09e94bdf138d7ca8db0425
boydovid <boybandovid@gmail.com>
2021-12-14 17:19:31 +0700

    update_env

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,9 +38,9 @@ export default {
     linkExactActiveClass: 'active'
   },
 
-  server: {        
-    host: process.env.BASE_URL, // default: localhost   
-  }, 
+  env: {
+    baseUrl: process.env.BASE_URL
+  },
   /*
   ** Customize the progress-bar color
   */
@@ -103,17 +103,17 @@ export default {
   },
 
   axios: {
-    // proxy: true,
+    proxy: true,
     credentials: true,
     baseURL: process.env.API_URL,
   },
 
-  // proxy: {
-  //   '/laravel': {
-  //     target: 'https://laravel-auth.nuxtjs.app',
-  //     pathRewrite: { '^/laravel': '/' }
-  //   }
-  // },
+  proxy: {
+    '/laravel': {
+      target: 'https://laravel-auth.nuxtjs.app',
+      pathRewrite: { '^/laravel': '/' }
+    }
+  },
 
   auth: {
     strategies: {
